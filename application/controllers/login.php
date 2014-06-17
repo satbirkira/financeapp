15f55c6e20483b7b0839302b5093aa56cd5c4519
Mukund Salia <salia.mukund@gmail.com>
2014-06-17 14:47:10 -0400

    Start Code for Login Page
    application/controller/login.php -> controller for login page
    application/views/login.php -> view for login page (HTML)
    application/models/login_model.php -> model for login functionality

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Login extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -19,9 +19,9 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('login');
 	}
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file login.php */
+/* Location: ./application/controllers/login.php */
