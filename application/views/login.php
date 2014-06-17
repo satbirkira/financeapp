a14fda9482b1e80d7c3476c7f007cf6f625c815a
Satbir Saini <satbir.kira@gmail.com>
2014-06-17 15:36:38 -0400

    added form skeleton

--- application/views/login.php
+++ application/views/login.php
@@ -3,11 +3,50 @@
 <head>
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
+	<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/md5.js"></script>
 </head>
 <body>
 
 <div id="container">
-LOGIN PAGE
+	<?php
+		//login is the controller, signin is the function
+		//setup arrays
+		$form = array(
+			'class' => 'form',
+			'id' => 'signin',
+			);
+		$username = array(
+              'name'        => 'username',
+              'id'          => 'username',
+			  'type'		=> 'text',
+              'value'       => '',
+              'maxlength'   => '11',
+              'size'        => '50',
+            );
+		$password = array(
+              'name'        => 'password',
+              'id'          => 'password',
+			  'type'		=> 'password',
+              'value'       => '',
+              'maxlength'   => '11',
+              'size'        => '50',
+            );
+		$labels = array(
+				'class' 	=> 'label',
+				'style' 	=> 'color: #000;',
+			);
+		$submit = array(
+				'class' 	=> 'submit',
+				'id'		=> 'submit_signin',
+			);
+		echo form_open('login/signin', $form);
+		echo form_label('Username: ', 'username', $labels);
+		echo form_input($username);
+		echo form_label('Password: ', 'password', $labels);
+		echo form_input($password);
+		echo form_submit('submit', 'Signin', $submit);
+		echo form_close();
+	?>
 </div>
 
 </body>
