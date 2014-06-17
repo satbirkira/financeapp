2ef4d8f9f98e9eaf551c8493fae3369cce2ad188
Mukund Salia <salia.mukund@gmail.com>
2014-06-17 14:34:39 -0400

    Adding CodeIgniter Base
    config, autoload files have been updated

---
+++ application/logs/log-2014-06-17.php
@@ -0,0 +1,51 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2014-06-17 20:26:23 --> Config Class Initialized
+DEBUG - 2014-06-17 20:26:23 --> Hooks Class Initialized
+DEBUG - 2014-06-17 20:26:23 --> Utf8 Class Initialized
+DEBUG - 2014-06-17 20:26:23 --> UTF-8 Support Enabled
+DEBUG - 2014-06-17 20:26:23 --> URI Class Initialized
+DEBUG - 2014-06-17 20:26:23 --> Router Class Initialized
+DEBUG - 2014-06-17 20:26:23 --> No URI present. Default controller set.
+DEBUG - 2014-06-17 20:26:23 --> Output Class Initialized
+DEBUG - 2014-06-17 20:26:23 --> Security Class Initialized
+DEBUG - 2014-06-17 20:26:23 --> Input Class Initialized
+DEBUG - 2014-06-17 20:26:23 --> XSS Filtering completed
+DEBUG - 2014-06-17 20:26:23 --> CRSF cookie Set
+DEBUG - 2014-06-17 20:26:23 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-06-17 20:26:23 --> Language Class Initialized
+DEBUG - 2014-06-17 20:26:23 --> Loader Class Initialized
+DEBUG - 2014-06-17 20:26:23 --> Database Driver Class Initialized
+DEBUG - 2014-06-17 20:26:23 --> Session Class Initialized
+DEBUG - 2014-06-17 20:26:23 --> Helper loaded: string_helper
+DEBUG - 2014-06-17 20:26:23 --> Encrypt Class Initialized
+DEBUG - 2014-06-17 20:26:23 --> A session cookie was not found.
+DEBUG - 2014-06-17 20:26:23 --> Session routines successfully run
+ERROR - 2014-06-17 20:26:23 --> Unable to load the requested class: forms
+DEBUG - 2014-06-17 20:27:01 --> Config Class Initialized
+DEBUG - 2014-06-17 20:27:01 --> Hooks Class Initialized
+DEBUG - 2014-06-17 20:27:01 --> Utf8 Class Initialized
+DEBUG - 2014-06-17 20:27:01 --> UTF-8 Support Enabled
+DEBUG - 2014-06-17 20:27:01 --> URI Class Initialized
+DEBUG - 2014-06-17 20:27:01 --> Router Class Initialized
+DEBUG - 2014-06-17 20:27:01 --> No URI present. Default controller set.
+DEBUG - 2014-06-17 20:27:01 --> Output Class Initialized
+DEBUG - 2014-06-17 20:27:01 --> Security Class Initialized
+DEBUG - 2014-06-17 20:27:01 --> Input Class Initialized
+DEBUG - 2014-06-17 20:27:01 --> XSS Filtering completed
+DEBUG - 2014-06-17 20:27:01 --> XSS Filtering completed
+DEBUG - 2014-06-17 20:27:01 --> XSS Filtering completed
+DEBUG - 2014-06-17 20:27:01 --> CRSF cookie Set
+DEBUG - 2014-06-17 20:27:01 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-06-17 20:27:01 --> Language Class Initialized
+DEBUG - 2014-06-17 20:27:01 --> Loader Class Initialized
+DEBUG - 2014-06-17 20:27:01 --> Helper loaded: form_helper
+DEBUG - 2014-06-17 20:27:01 --> Database Driver Class Initialized
+DEBUG - 2014-06-17 20:27:02 --> Session Class Initialized
+DEBUG - 2014-06-17 20:27:02 --> Helper loaded: string_helper
+DEBUG - 2014-06-17 20:27:02 --> Encrypt Class Initialized
+DEBUG - 2014-06-17 20:27:02 --> Session routines successfully run
+DEBUG - 2014-06-17 20:27:02 --> Controller Class Initialized
+DEBUG - 2014-06-17 20:27:02 --> File loaded: application/views/welcome_message.php
+DEBUG - 2014-06-17 20:27:02 --> Final output sent to browser
+DEBUG - 2014-06-17 20:27:02 --> Total execution time: 0.0303
