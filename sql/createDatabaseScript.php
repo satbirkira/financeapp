96241a7f08466e27bf6556d10a65c408f835428b
Satbir Saini <satbir.kira@gmail.com>
2014-06-20 16:41:34 -0400

    added history table

--- sql/createDatabaseScript.php
+++ sql/createDatabaseScript.php
@@ -108,6 +108,31 @@
 	if (!mysqli_select_db($link, $database_name)) die("Could not access database: ". mysqli_error($link));
 
 	
+	
+	/*
+	
+		Create History table
+	
+	*/
+	
+	
+
+	echo "Creating History..</br>";
+	$query = file_get_contents("table_history.sql");
+	
+	if(!mysqli_multi_query($link, $query)) die("Could not create table: " . mysqli_error($link));
+	
+	//need to close connection and restart after mysqli_multi_query is required
+	mysqli_close($link);
+	$link = mysqli_connect(
+			$location, 
+			$username,
+			$pass
+		);
+	if (!$link) die("Could not connect: ". mysqli_error($link));
+	if (!mysqli_select_db($link, $database_name)) die("Could not access database: ". mysqli_error($link));
+	
+	
 	/*
 	
 		Insert example user
@@ -193,6 +218,27 @@
 	echo "-------------------------------</br>";
 	
 	$query = "select * from account";
+	$result = mysqli_query($link, $query);
+	if(!$result) die("Could not select accounts: " . mysqli_error($link));
+
+	$i = 0;
+	while ($record = mysqli_fetch_assoc ($result))
+    {
+		echo "Account Number $i by : ". $record['userID']. "</br>";
+		foreach ($record as $column => $data)
+		{
+			echo "$column : $data</br>";
+		}
+		echo "==</br>";
+	}
+	
+	
+	
+	echo "-------------------------------</br>";
+	echo "Displaying History Table</br>";
+	echo "-------------------------------</br>";
+	
+	$query = "select * from history";
 	$result = mysqli_query($link, $query);
 	if(!$result) die("Could not select accounts: " . mysqli_error($link));
 
