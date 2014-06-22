03bd25d99a7e79fc01aaffaf3d88ee7265e67840
zs9912 <shu.zhang@yahoo.com>
2014-06-21 22:54:13 -0400

    update the Goal table

--- sql/table_goal.sql
+++ sql/table_goal.sql
@@ -1,13 +1,19 @@
 DROP TABLE IF EXISTS `financeapp`.`goal`;
-CREATE TABLE  `financeapp`.`goal` (
-  `goalID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `userID` int(10) NOT NULL, 
-  `goalName` varchar(45) NOT NULL,
+CREATE TABLE `financeapp`.`goal` (
+  `goalID` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `userID` INTEGER UNSIGNED NOT NULL,
+  `goalName` VARCHAR(45) NOT NULL,
   `startDate` DATE NOT NULL,
   `targetDate` DATE NOT NULL,
-  `totalCost` int(10) NOT NULL DEFAULT 0,
-  `currentlySaved` int(10) NOT NULL DEFAULT 0,
-  `goalDeleted` BOOLEAN DEFAULT FALSE,
+  `totalCost` DECIMAL NOT NULL DEFAULT 0.00,
+  `monthlyDepot` DECIMAL DEFAULT 0.00,
+  `interestRate` DECIMAL DEFAULT 0.00,
+  `currentlySaved` DECIMAL DEFAULT 0.00,
+  `goalStatus` BOOLEAN DEFAULT false,
   PRIMARY KEY (`goalID`),
-  FOREIGN KEY (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=gb2312 ROW_FORMAT=DYNAMIC;
+  CONSTRAINT `FK_goal_1` FOREIGN KEY `FK_goal_1` (`userID`)
+    REFERENCES `user` (`userID`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT
+)
+ENGINE = InnoDB;
