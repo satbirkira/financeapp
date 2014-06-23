231d0c6a5eb1f3293af10be6bc7c4d3e0d8d4706
zs9912 <shu.zhang@yahoo.com>
2014-06-23 13:43:55 -0400

    update goal model

--- application/models/goal_model.php
+++ application/models/goal_model.php
@@ -1,18 +1,44 @@
 <?php
-class Goal_model extends CI_Model {
+class Goal_Model extends CI_Model {
 	
-
+	var $goalTitle = "";
+	var $total = "";
+	var $startDate = "";
+	var $targetDate = "";
+	var $monthlyDepot = "";	
+	var $interestRate = "";	
 
     function __construct()
     {
-		
+        // Call the Model constructor
+        parent::__construct();
     }
-	
-	function add_goal()
+
+	function create_new_goal($arrGoal)
     {
-		
+		$result = $this->db->insert('goal',$arrGoal);
+		//$goalId = $this->db->insert_id();		
+		return $result;
     }
+		
+	function get_goal_details($gid)
+	{
+		$this->db->select('*');
+		$this->db->from('goal');
+		$this->db->where('goalId',$uid);
+		$query = $this->db->get();
+		return $query->result();
+	}
 	
+	function get_all_goal($uid)
+	{
+		$this->db->select('*');
+		$this->db->from('goal');
+		$this->db->where('userId',$uid);
+		$query = $this->db->get();
+		return $query->result();
+	}
+
 	function remove_goal()
     {
 		
