97c924e9b6c92001b376c29590cac449ec8431a6
Mukund Salia <salia.mukund@gmail.com>
2014-07-04 16:46:45 -0400

    UI update: Dashboard design almost ready

--- application/views/dashBoard.php
+++ application/views/dashBoard.php
@@ -2,5 +2,72 @@
 	Your dashboard.
 </div>
 <div id="template_content_container">
-<?=$variable1?>
+	<div id="dash_metrics">
+		<div class="dash_big_display blue">
+			<div class="amount">
+				<span class="currency_sym">$</span><span class="dash_value">2000</span>
+			</div>
+			<div class="description">Budget</div>
+		</div>
+
+		<div class="dash_big_display blue">
+			<div class="amount">
+				<span class="currency_sym">$</span><span class="dash_value">200</span>
+			</div>
+			<div class="description">Expenditure</div>
+		</div>
+
+		<div class="dash_big_display red">
+			<div class="amount">
+				<span class="currency_sym">#</span><span class="dash_value">21</span>
+			</div>
+			<div class="description">Outstanding Goals</div>
+		</div>
+
+		<div class="dash_big_display green">
+			<div class="amount">
+				<span class="currency_sym">#</span><span class="dash_value">3</span>
+			</div>
+			<div class="description">Goals Completed</div>
+		</div>
+
+		<div class="dash_big_display yellow last">
+			<div class="amount">
+				<span class="currency_sym">#</span><span class="dash_value">3</span>
+			</div>
+			<div class="description">Collaborative Goals</div>
+		</div>
+		<div style="clear: both"></div>
+	</div>
+
+<?if(true):?>
+	<div id="setting_title" style="margin: 30px 0px 15px 0px;">
+		<span style="font-size: 30px; font-weight: 300;">My goals</span>
+	</div>
+	<div id="dash_metrics">
+		<div class="dash_prog_display">
+			<div class="goal_title" style="float:left;">Goal #1 Title</div>
+			<div class="goal_percent" style="float:right;text-align: right;">50%</div>
+			<div class="goal_bar_outer">
+				<div class="goal_bar_inner" style="width: 50%"></div>
+			</div>
+		</div>
+		<div class="dash_prog_display">
+			<div class="goal_title" style="float:left;">Goal #2 Title</div>
+			<div class="goal_percent" style="float:right;text-align: right;">30%</div>
+			<div class="goal_bar_outer">
+				<div class="goal_bar_inner" style="width: 30%"></div>
+			</div>
+		</div>
+		<div class="dash_prog_display">
+			<div class="goal_title" style="float:left;">Goal #3 Title</div>
+			<div class="goal_percent" style="float:right;text-align: right;">63%</div>
+			<div class="goal_bar_outer">
+				<div class="goal_bar_inner" style="width: 63%"></div>
+			</div>
+		</div>
+		<div style="clear: both"></div>
+	</div>
+<?endif?>
+	<div style="clear: both"></div>
 </div>
